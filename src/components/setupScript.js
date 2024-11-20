a3dc191afb0feee4c9cecbdf8628bbd4998a5ddf
AlexJuraska <juraska.alex@gmail.com>
2024-11-20 23:33:19 +0100

    Made sure user can't move between steps on the stepper until they've set up properly

--- src/components/setupScript.js
+++ src/components/setupScript.js
@@ -8,19 +8,34 @@ const TOTAL_STEPS = 3;
  */
 function changeStep(direction) {
 
+    // Prevents moving from step2 to step 3 if it is not calibrated
+    if (direction === 1) {
+        if (currentStep === 2 && calibrationData.length === 0) {
+            alert("No calibration data to export. Please calibrate first.");
+            return;
+        } else if (currentStep === 1 && cameraUsed.trim().length === 0) {
+            alert("No available camera. Please connect a camera first.");
+            return;
+        }
+    }
+
     document.getElementById(`step${currentStep}`).classList.remove('active');   //Remove the previous step
     currentStep += direction;   // Add the following step
     document.getElementById(`step${currentStep}`).classList.add('active');  //Add the current step
 
-    if (currentStep === 1) {
-        videoElement = document.getElementById("videoSelect");
-        resetCamera();
-    } else if (currentStep === TOTAL_STEPS) {
-        videoElement = document.getElementById("videoMain");
+    if (currentStep === 1 || currentStep === TOTAL_STEPS) {
+        videoElement = document.getElementById(currentStep === 1 ? "videoSelect" : "videoMain");
         resetCamera();
     }
 
-    // Enable or disable buttons based on the current step
-    document.getElementById('prevButton').disabled = (currentStep === 1);
-    document.getElementById('nextButton').disabled = (currentStep === TOTAL_STEPS);
+    if (currentStep === 1) {
+        document.getElementById('prevButton').disabled = true;
+        document.getElementById('nextButton').disabled = false;
+    } else if (currentStep === 2) {
+        document.getElementById('prevButton').disabled = false;
+        document.getElementById('nextButton').disabled = false;
+    } else if (currentStep === 3) {
+        document.getElementById('prevButton').disabled = true;
+        document.getElementById('nextButton').disabled = true;
+    }
 }
